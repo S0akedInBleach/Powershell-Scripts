6f3adcebc2c61fd6560d6094b8af60f54130fb70
S0akedInBleach <46869654+S0akedInBleach@users.noreply.github.com>
2023-03-16 20:01:45 -0400

    Update PWSH_IP_Lookup.ps1

--- PWSH_IP_Lookup.ps1
+++ PWSH_IP_Lookup.ps1
@@ -1,23 +1,53 @@
-#use windows.forms to prompt user for IP address 
-
-$ip = [System.Windows.Forms.InputBox]::Show("Enter IP Address", "IP Lookup", "
-")
-
-$ip = $ip.Trim()
-
-if ($ip -eq $null) {
-    Write-Host "No IP address entered"
-    exit
+#function for windows.forms to collect $ip address from user via gui
+function Get-InputBox {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$Title,
+        [Parameter(Mandatory=$true)]
+        [string]$Prompt,
+        [Parameter(Mandatory=$true)]
+        [string]$DefaultResponse
+    )
+    Add-Type -AssemblyName System.Windows.Forms
+    $form = New-Object System.Windows.Forms.Form
+    $form.Text = $Title
+    $form.Width = 500
+    $form.Height = 150
+    $form.TopMost = $true
+    $form.FormBorderStyle = 'FixedDialog'
+    $form.MaximizeBox = $false
+    $form.MinimizeBox = $false
+    $form.StartPosition = 'CenterScreen'
+    $label = New-Object System.Windows.Forms.Label
+    $label.Text = $Prompt
+    $label.AutoSize = $true
+    $label.Location = New-Object System.Drawing.Point(10,10)
+    $textbox = New-Object System.Windows.Forms.TextBox
+    $textbox.Text = $DefaultResponse
+    $textbox.Location = New-Object System.Drawing.Point(10,30)
+    $textbox.Width = 470
+    $textbox.SelectAll()
+    $button = New-Object System.Windows.Forms.Button
+    $button.Text = 'OK'
+    $button.Location = New-Object System.Drawing.Point(10,60)
+    $button.Width = 470
+    $button.DialogResult = 'OK'
+    $form.AcceptButton = $button
+    $form.Controls.Add($label)
+    $form.Controls.Add($textbox)
+    $form.Controls.Add($button)
+    $form.ShowDialog() | Out-Null
+    $textbox.Text
 }
+
+$ip= Get-InputBox -Title "IP Lookup" -Prompt "Enter IP Address" -DefaultResponse "
 
 #use $ip to build URLs for lookup
 
-
-
-
 $urls = @(
     "https://www.abuseipdb.com/check/$ip"
-    "https://www.virustotal.com/gui/ip-address/$ip/details"
+    "https://www.virustotal.com/gui/ip-address/$ip/details
     "https://exchange.xforce.ibmcloud.com/ip/$ip"
     "https://www.shodan.io/host/$ip"
     "https://otx.alienvault.com/indicator/ip/$ip"
