edc5f289a5c26d1b4744ce44e01db95ffdd86d1b
S0akedInBleach <46869654+S0akedInBleach@users.noreply.github.com>
2023-03-21 19:38:16 -0400

    Update Database_auth_query.ps1

--- Database_auth_query.ps1
+++ Database_auth_query.ps1
@@ -1,3 +1,4 @@
+#capture string as a base search term for codys.webpage.com
 $codys_the_man = Read-Host "Enter a string"
 
 # List of top TLDs to check
@@ -9,7 +10,7 @@ foreach ($tld in $tlds) {
     $url = "https://codys.webpage.com/Dashboard.aspx?D=0&k=$domain"
     $wwwUrl = "https://codys.webpage.com/Dashboard.aspx?D=0&k=$wwwDomain"
 
-    # Check if Record Count is greater than 0 for non-www domain
+    #check for the inital string given first using Microsoft Edge
     $page = Invoke-WebRequest -Uri $url -UseDefaultCredentials
     $recordCount = $page.ParsedHtml.getElementById("MainContent_iblRecordCount")
 
@@ -23,13 +24,35 @@ foreach ($tld in $tlds) {
                 # Launch the browser with the URL
                 Start-Process $url
             }
+            else {
+                # Open the URL in the existing browser session
+                Start-Process -FilePath "msedge" -ArgumentList "--new-window $url"
+            }
         }
     }
-    else {
-        Write-Host "Error: Could not find Record Count for domain: $domain"
+    
+    # Check if Record Count is greater than 0 for non-www domain using Microsoft Edge
+    $page = Invoke-WebRequest -Uri $url -UseDefaultCredentials
+    $recordCount = $page.ParsedHtml.getElementById("MainContent_iblRecordCount")
+
+    if ($recordCount -ne $null -and $recordCount.InnerText -match "Record Count: (\d+)") {
+        $count = [int]$Matches[1]
+        Write-Host "Domain: $domain, Record Count: $count"
+        if ($count -gt 0) {
+            # Check for an existing browser session
+            $browserProcesses = Get-Process -Name "msedge" -ErrorAction SilentlyContinue
+            if ($browserProcesses.Count -eq 0) {
+                # Launch the browser with the URL
+                Start-Process $url
+            }
+            else {
+                # Open the URL in the existing browser session
+                Start-Process -FilePath "msedge" -ArgumentList "--new-window $url"
+            }
+        }
     }
 
-    # Check if Record Count is greater than 0 for www domain
+    # Check if Record Count is greater than 0 for www domain using Microsoft Edge
     $page = Invoke-WebRequest -Uri $wwwUrl -UseDefaultCredentials
     $recordCount = $page.ParsedHtml.getElementById("MainContent_iblRecordCount")
 
@@ -43,9 +66,23 @@ foreach ($tld in $tlds) {
                 # Launch the browser with the URL
                 Start-Process $wwwUrl
             }
+            else {
+                # Open the URL in the existing browser session
+                Start-Process -FilePath "msedge" -ArgumentList "--new-window $wwwUrl"
+            }
         }
     }
-    else {
-        Write-Host "Error: Could not find Record Count for domain: $wwwDomain"
-    }
 }
+
+
+
+
+
+
+
+
+
+
+
+   
+            
